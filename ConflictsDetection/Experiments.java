8d29410b8fa588baac7b6c6f084c46e1551bc38d
riqueGo <henrique.oliveira.gomes@gmail.com>
2024-02-29 22:27:45 -0300

    reorder tests

--- ConflictsDetection/Experiments.java
+++ ConflictsDetection/Experiments.java
@@ -16,9 +16,11 @@ public class Experiments {
         System.out.println(message);
     }
     
-    //Merge program
-    //left changed the balance in constructor
-    //right use it to build the message
+    /*  
+     * Example Merge program
+     * left changed the balance in constructor
+     * right use it to build the message
+     */
     public void leftDefAndRightUseItMerge() {
         Account acc = new Account("1", 100, AccountType.SAVING); // left
         base();
@@ -29,10 +31,13 @@ public class Experiments {
         System.out.println(message);
     }
 
-    //Merge program to Infer analysis
-    //Left change has been extracted
-    //Whole Right attribution was extracted
-    //There is a conflict since Left change balance and right use it (Data Flow)
+    //############################### Conflicts ###############################
+
+    /*  
+     * Left change has been extracted
+     * Whole Right attribution was extracted
+     * There is a conflict since Left change balance and right use it (Data Flow)
+     */
     public void leftDefAndRightUseItInferConflict() {
         Account acc = new Account("1", Infer.defLeft(100), AccountType.SAVING); // left
         base();
@@ -44,41 +49,11 @@ public class Experiments {
         System.out.println(message);
     }
 
-    //Alternative Merge program to Infer analysis
-    //Left change has been extracted
-    //Whole Right attribution was extracted
-    //There isn't a conflict since Left change balance and right used accountType
-    public void leftDefAndRightUseItInferNotConflict() {
-        Account acc = new Account("1", Infer.defLeft(100), AccountType.SAVING); // left
-        base();
-        String message = Infer.useRight(new StringBuilder()
-            .append("Account: ").append(acc.getAccountNumber())
-            .append("\nType: ").append(acc.getAccountType()) //right
-            .toString()
-        );
-        System.out.println(message);
-    }
-
-    //Alternative Merge program to Infer analysis
-    //Left change has been extracted
-    //Whole Right attribution was extracted
-    //There isn't a conflict since base has sanitized balance
-    public void leftDefBaseDefAndRightUseItInferNotConflict() {
-        Account acc = new Account("1", Infer.defLeft(100), AccountType.SAVING); // left
-        base();
-        acc.setBalance(acc.getBalance()+100); //base
-        String message = Infer.useRight(new StringBuilder()
-            .append("Account: ").append(acc.getAccountNumber())
-            .append("\nBalance: ").append(acc.getBalance()) //right
-            .toString()
-        );
-        System.out.println(message);
-    }
-
-    //Alternative Merge program to Infer analysis
-    //Left change has been extracted
-    //Since right has change the balance by a method, the whole method was extracted to rightExtracted
-    //There is a conflict since left behavior program was changed
+    /*  
+     * Left change has been extracted
+     * Since right has change the balance by a method, the whole method was extracted to rightExtracted
+     * There is a conflict since left behavior program was changed
+     */
     public void leftDefRightDefAndLeftUseItInferConflict() {
         Account acc = new Account("1", Infer.defLeft(100), AccountType.SAVING); // left
         base();
@@ -91,11 +66,56 @@ public class Experiments {
         System.out.println(message);
     }
 
-    //Alternative Merge program to Infer analysis
-    //Left change has been extracted
-    //Since right has change the balance by a method, the whole method was extracted to rightExtracted
-    //However left doesn't use the value changed,
-    //so there is no conflict since left and right behavior was preserved
+    //conflict detected with nested objects
+    public void leftDefAndRightUseItInferConflictWithPerson() {
+        Account acc = new Account("1", 0, AccountType.SAVING);
+        Person person = new Person("Henrique", Infer.defLeft(24), "Rua do Sol"); //left
+        acc.setOwner(person);
+        base();
+        System.out.println(Infer.useRight(acc.getOwner().getAge())); //right
+    }
+
+    //############################### Not Conflict ###############################
+
+    /*  
+     * Left change has been extracted
+     * Whole Right attribution was extracted
+     * There isn't a conflict since Left change balance and right used accountType
+     */
+    public void leftDefAndRightUseItInferNotConflict() {
+        Account acc = new Account("1", Infer.defLeft(100), AccountType.SAVING); // left
+        base();
+        String message = Infer.useRight(new StringBuilder()
+            .append("Account: ").append(acc.getAccountNumber())
+            .append("\nType: ").append(acc.getAccountType()) //right
+            .toString()
+        );
+        System.out.println(message);
+    }
+
+    /*  
+     * Left change has been extracted
+     * Whole Right attribution was extracted
+     * There isn't a conflict since base has sanitized balance
+     */
+    public void leftDefBaseDefAndRightUseItInferNotConflict() {
+        Account acc = new Account("1", Infer.defLeft(100), AccountType.SAVING); // left
+        base();
+        acc.setBalance(acc.getBalance()+100); //base
+        String message = Infer.useRight(new StringBuilder()
+            .append("Account: ").append(acc.getAccountNumber())
+            .append("\nBalance: ").append(acc.getBalance()) //right
+            .toString()
+        );
+        System.out.println(message);
+    }
+
+    /*
+     * Left change has been extracted
+     * Since right has change the balance by a method, the whole method was extracted to rightExtracted
+     * However left doesn't use the value changed,
+     * so there is no conflict since left and right behavior was preserved
+     */
     public void leftDefRightDefAndLeftUseItInferNotConflict() {
         Account acc = new Account("1", Infer.defLeft(100), AccountType.SAVING); // left
         base();
@@ -112,15 +132,6 @@ public class Experiments {
         acc.setBalance(100);
     }
 
-    //conflict detected with nested objects
-    public void leftDefAndRightUseItInferConflictWithPerson() {
-        Account acc = new Account("1", 0, AccountType.SAVING);
-        Person person = new Person("Henrique", Infer.defLeft(24), "Rua do Sol"); //left
-        acc.setOwner(person);
-        base();
-        System.out.println(Infer.useRight(acc.getOwner().getAge())); //right
-    }
-
     //there wasn't a conflict since right uses a different field
     public void leftDefAndRightUseItInferNotConflictWithPerson() {
         Account acc = new Account("1", 0, AccountType.SAVING);
@@ -129,6 +140,8 @@ public class Experiments {
         base();
         System.out.println(Infer.useRight(acc.getOwner().getName())); //right
     }
+
+    //############################### It should has a conflict ###############################
 
     //It should have a conflict however Infer doesn't catch
     public void leftDefAndRightUseItByFunctionInferConflict() {
